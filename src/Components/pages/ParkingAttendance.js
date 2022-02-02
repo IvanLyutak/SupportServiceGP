3b216a4f8f5037f4c5a4b365687f68c97f7b2e47
Павел Пашута <popashuta@miem.hse.ru>
2022-02-03 00:16:00 +0300

    convert from UTC to local

--- src/Components/pages/ParkingAttendance.js
+++ src/Components/pages/ParkingAttendance.js
@@ -18,10 +18,35 @@ class ParkingAttendance extends React.Component{
 
         onValue(starCountRef, (snapshot) => {
             const data = Object.keys(snapshot.val()).map(k=>(snapshot.val()[k]))
+
+
             for (const elem of data) {
+            
               elem["time_reservation"] = (elem["time_reservation"]).split('.')[0]
               elem["time_arrive"] = (elem["time_arrive"]).split('.')[0]
               elem["time_exit"] = (elem["time_exit"]).split('.')[0]
+
+              var pad = function(i) { return (i < 10) ? '0' + i : i; };
+
+              var local_time_reservation = new Date(elem["time_reservation"] + ' UTC');
+              var local_time_arrive = new Date(elem["time_arrive"] + ' UTC');
+              var local_time_exit = new Date(elem["time_exit"] + ' UTC');
+
+              elem["time_reservation"] = local_time_reservation.getFullYear() + '-' +
+                pad(local_time_reservation.getMonth() + 1) + '-' + pad(local_time_reservation.getDate()) + ' '
+                + pad(local_time_reservation.getHours()) + ':' + pad(local_time_reservation.getMinutes()) + ':' +
+                pad(local_time_reservation.getSeconds());
+
+              elem["time_arrive"] = local_time_arrive.getFullYear() + '-' +
+                pad(local_time_arrive.getMonth() + 1) + '-' + pad(local_time_arrive.getDate()) + ' '
+                + pad(local_time_arrive.getHours()) + ':' + pad(local_time_arrive.getMinutes()) + ':' +
+                pad(local_time_arrive.getSeconds());
+
+              elem["time_exit"] = local_time_exit.getFullYear() + '-' +
+                pad(local_time_exit.getMonth() + 1) + '-' + pad(local_time_exit.getDate()) + ' '
+                + pad(local_time_exit.getHours()) + ':' + pad(local_time_exit.getMinutes()) + ':' +
+                pad(local_time_exit.getSeconds());
+
             }
             this.setState({
                 data_table: {
